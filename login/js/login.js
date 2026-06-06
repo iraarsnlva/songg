b369e59ac7a3ddb65a2604a996b279db02701490
iraarsnlva <leiviaira@gmail.com>
2026-06-06 11:08:46 +0700

    Update login.js

--- login/js/login.js
+++ login/js/login.js
@@ -30,7 +30,7 @@ document.getElementById("loginForm").addEventListener("submit", async function(e
 
     setTimeout(() => {
         alertBox.style.display = "none";
-    }, 2600);
+    }, 2450);
 } 
    
 });
